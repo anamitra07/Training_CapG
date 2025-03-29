d78b04da36bcc5b56531e9ec0624bc9d3ed95e02
UEM <anamitra07>
2025-03-29 14:25:30 +0530

    files updated

---
+++ March_29th/AbstractVehicle.java
@@ -0,0 +1,7 @@
+package March_29th;
+
+abstract class AbstractVehicle implements Vehicle {
+    protected String name;
+    protected int speed;
+
+}
---
+++ March_29th/Vehicle.java
@@ -0,0 +1,10 @@
+package March_29th;
+
+public interface Vehicle {
+    void start();
+    void stop();
+    void accelerate();
+    void brake();
+    void getCurrentSpeed();
+    void displayType();
+}
