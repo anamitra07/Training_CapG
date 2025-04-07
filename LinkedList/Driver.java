f42f7b19c54f327762133ee9dd5400eca16bd6c1
UEM <ishankanjilal@gmail.com>
2025-04-07 16:23:48 +0530

    updated files

--- LinkedList/Driver.java
+++ LinkedList/Driver.java
@@ -1,4 +1,57 @@
 package LinkedList;
 
+import java.util.Scanner;
+
 public class Driver {
+    static int pos, val;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Singly_Llist list = new Singly_Llist();
+        boolean flag = true;
+        while(flag) {
+            System.out.println("\n1. Insert data\n2. Update data\n3. Insert at a specific position\n4. Delete from a specific position\n5. Display data\n6. Exit");
+            System.out.print("Enter your choice: ");
+            int choice = sc.nextInt();
+            switch (choice) {
+                case 1: {
+                    System.out.print("\nEnter the data: ");
+                    int data = sc.nextInt();
+                    list.insert(data);
+                    break;
+                }
+                case 2: {
+                    System.out.print("\nEnter position: ");
+                    pos = sc.nextInt();
+                    System.out.print("\nEnter value: ");
+                    val = sc.nextInt();
+                    list.update(pos, val);
+                    break;
+                }
+                case 3: {
+                    System.out.print("\nEnter position to insert: ");
+                    pos = sc.nextInt();
+                    System.out.print("\nEnter value to insert: ");
+                    val = sc.nextInt();
+                    list.insert_at_position(pos, val);
+                    break;
+                }
+                case 4: {
+                    System.out.print("\nEnter position to delete from: ");
+                    pos = sc.nextInt();
+                    list.delete(pos);
+                    break;
+                }
+                case 5:
+                    list.display();
+                    break;
+
+                case 6:
+                    flag = false;
+                    break;
+
+                default:
+                    System.err.println("\nInvalid choice!!");
+            }
+        }
+    }
 }
--- LinkedList/Singly_Llist.java
+++ LinkedList/Singly_Llist.java
@@ -1,7 +1,117 @@
 package LinkedList;
 
-public class Singly {
-    public static void main(String[] args) {
+public class Singly_Llist {
+    Node head;
+    Node tail;
+    static int count;
 
+    public void insert(int data) {
+
+        // creating a node object;
+        Node node = new Node(data);
+
+        // for first node object
+        if(head==null) {
+            head = node;
+            tail = node;
+            count++;
+        }
+
+        // for every next node object
+        else {
+            tail.next = node;
+            tail = node;
+            count++;
+        }
+    }
+
+    public void insert_at_position(int pos, int value) {
+
+        // checking if position is within the size or not
+        if(pos<1 || pos>count+1)
+            System.err.println("\nInvalid position for insertion.");
+        else {
+            // creating a new node, named node.
+            Node node = new Node(value);
+
+            // creating a pointer object to traverse through the linked list.
+            Node ptr = this.head;
+
+            // checking if any element is present in the list or not
+            if(head==null) {
+                head = node;
+                tail = node;
+                count++;
+                return;
+            }
+
+            // if the node is inserted at position 1, its next part will point to head
+            if (pos == 1) {
+                node.next = head;
+                head = node;
+            }
+            else {
+                // for any other case
+                int currIndex = 1;
+                while (ptr != null && currIndex < pos - 1) {
+                    ptr = ptr.next;
+                    currIndex++;
+                }
+
+                node.next = ptr.next;
+                ptr.next = node;
+            }
+        }
+    }
+
+    public void update(int pos,int value) {
+
+        // creating a pointer 'ptr' for traversing through the list
+        Node ptr = this.head;
+        if(pos>count)
+            System.err.println("\nNo data present at specified position.");
+        else {
+            for(int currIndex = 1 ; currIndex <= count ; currIndex++) {
+                if (currIndex == pos)
+                    ptr.data = value;
+                ptr = ptr.next;
+            }
+        }
+    }
+
+    public void delete(int pos) {
+
+        if(pos<1 || pos>count) {
+            System.err.println("\nNo data present!!");
+        }
+
+        // creating a pointer 'ptr' for traversing through the list
+        Node ptr = this.head;
+
+        int currIndex=1;
+        if(pos==1) {
+            ptr = ptr.next;
+            head = ptr;
+            return;
+        }
+
+        while(ptr!=null && currIndex<pos-1) {
+            ptr = ptr.next;
+            currIndex++;
+        }
+        ptr.next = ptr.next.next;
+    }
+
+    public void display() {
+        Node ptr = this.head;
+        if(ptr!=null) {
+            System.out.print("\nElements are: ");
+            while (ptr != null) {
+                System.out.print(ptr.data + " ");
+                ptr = ptr.next;
+            }
+        }
+        else
+            System.err.println("\nNo data present");
     }
 }
